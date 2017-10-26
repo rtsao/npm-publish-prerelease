c8d502d74e0fd6c7d3cc908a280bc2848d9dac60
Ryan Tsao <ryan.j.tsao@gmail.com>
2017-10-25 22:29:15 -0700

    Initial implementation

---
+++ npm-publish-prerelease.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+const {join} = require('path');
+const {spawn} = require('child_process');
+const semver = require('semver');
+
+const pkg = require(join(process.cwd(), 'package.json'));
+
+const {version} = pkg;
+if (!semver.valid(version)) {
+  console.error(`Error: package version "${version}" is invalid.`);
+  process.exit(1);
+}
+
+const {prerelease} = semver(version);
+
+const tag = prerelease.length
+  ? prerelease.filter(part => typeof part === 'string').join('.') ||
+    'prerelease'
+  : void 0;
+
+const args = process.argv.slice(2);
+const tagArgs = tag ? ['--tag', tag] : [];
+
+spawn('npm', ['publish', ...args, ...tagArgs], {
+  stdio: 'inherit'
+});
